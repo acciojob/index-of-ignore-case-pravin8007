2d0986cb05540048b273538822b828bab41a5a92
Pravin Patil <106026547+pravin8007@users.noreply.github.com>
2023-12-13 13:34:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
-	 if (str.length === 0 || subStr.length === 0) {
+	 if (s1.length === 0 || s2.length === 0) {
 		    return -1;
 	 }
-  const lowerStr = str.toLowerCase();
-  const lowerSubStr = subStr.toLowerCase();
+  const lowerStr = s1.toLowerCase();
+  const lowerSubStr = s2.toLowerCase();
   
    const index = lowerStr.indexOf(lowerSubStr);
 	return index ;
+	
 }
 
-// Please do not change the code below
+// // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(indexOfIgnoreCase(s1, s2));
+
